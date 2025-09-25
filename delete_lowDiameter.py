e5d8959e492af0425c70972f925f8ea61d89234b
Mohammadmehdi Moghtadaei <126883521+mohammadmehdimoghtadaei@users.noreply.github.com>
2025-09-25 19:26:55 +0330

    Update delete_lowDiameter.py

--- delete_lowDiameter.py
+++ delete_lowDiameter.py
@@ -9,4 +9,4 @@ condition = df[column_name] < 6.4
 df = df[~condition]  # Use ~ to negate the condition and keep the rows where the condition is False
 
 # Export the modified data to a new CSV file
-df.to_csv('filtered_file1.csv', index=False)
+df.to_csv('ouput.csv', index=False)
